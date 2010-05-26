ceaeda64099af7b40167d6d436866bba65394afc
angelng <angel.ng.27@gmail.com>
2010-05-26 18:41:45 -0400

    Add WP Geo Map Plugin

--- functions.php
+++ functions.php
@@ -1,25 +1,11 @@
 <?php
 
 //
-//  Custom Child Theme Functions
+//  Upgrade Node Custom Functions for Thematic Framework
 //
 
-// I've included a "commented out" sample function below that'll add a home link to your menu
 // More ideas can be found on "A Guide To Customizing The Thematic Theme Framework" 
 // http://themeshaper.com/thematic-for-wordpress/guide-customizing-thematic-theme-framework/
-
-// Adds a home link to your menu
-// http://codex.wordpress.org/Template_Tags/wp_page_menu
-//function childtheme_menu_args($args) {
-//    $args = array(
-//        'show_home' => 'Home',
-//        'sort_column' => 'menu_order',
-//        'menu_class' => 'menu',
-//        'echo' => true
-//    );
-//	return $args;
-//}
-//add_filter('wp_page_menu_args','childtheme_menu_args');
 
 
 // Clean up the Dashboard
@@ -59,6 +45,184 @@ unregister_sidebar('page-bottom');
 
 // When WP initiates, add the above settings
 add_action( 'init', 'childtheme_sidebars_init',20 );
+
+
+// Add Blueprint CSS and WP Geo into wp_head
+function add_wphead() {
+
+ // Include main screen styles css
+ $content .= "\t";
+ $content .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"";
+ $content .= get_bloginfo('stylesheet_directory');
+ $content .= '/styles/grid.css';
+ $content .= "\" media=\"grid\" />";
+ $content .= "\n";
+
+ // Include print css
+ $content .= "\t";
+ $content .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"";
+ $content .= get_bloginfo('stylesheet_directory');
+ $content .= '/styles/print.css';
+ $content .= "\" media=\"print\" />";
+ $content .= "\n";
+ 
+ // Include IE-specific CSS fix
+ $content .= "\t";
+ $content .= "<!--[if lt IE 8]><link rel=\"stylesheet\" type=\"text/css\" href=\"";
+ $content .= get_bloginfo('stylesheet_directory');
+ $content .= '/styles/ie.css';
+ $content .= "\" /><![endif]-->";
+ $content .= "\n";
+ 
+ // Include the original style.css again so it overides the blueprint code
+ // Ideally we would've also found a way to remove the first reference to styles.css. If there's a better way, please share.
+ $content .= "\t";
+ $content .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"";
+ $content .= get_bloginfo('stylesheet_url');
+ $content .= "'\" media=\"style\" />";
+ $content .= "\n";
+ 
+ // Include any other stylesheet you are using
+ $content .= "\t";
+ $content .= "<link rel=\"stylesheet\" type=\"text/css\" href=\"";
+ $content .= get_bloginfo('stylesheet_directory');
+ $content .= '/styles/upgradethematic.css';
+ $content .= "\" media=\"upgradethematic\" />";
+ $content .= "\n";
+ 
+ // Echo the whole thing
+ echo $content;
+ 
+// Add gmap scripts and options
+   if(get_option('upgrades_use_gmap')): ?>
+	
+	  <?php $wpgeo_options = get_option("wp_geo_options"); ?>
+	  <script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=<?=$wpgeo_options['google_api_key']?>" type="text/javascript"></script>
+	    <script type="text/javascript">
+	    //<![CDATA[
+		var map;
+	    function load()
+		{
+			if (GBrowserIsCompatible())
+			{
+				<?php
+				
+				$lat = null;
+				$lon = null;
+				
+				$my_query = new WP_Query("category_name=events&showposts=1");
+				if($my_query->have_posts())
+				{
+					$my_query->the_post();
+					$lon = get_post_meta($my_query->post->ID, '_wp_geo_longitude', true);
+					$lat = get_post_meta($my_query->post->ID, '_wp_geo_latitude', true);
+				}
+				
+				if(!$lat || !$lon)
+				{
+					$lat = attribute_escape(upgrades_node_lat());
+					$lon = attribute_escape(upgrades_node_lon());
+				}
+				$zoom = attribute_escape(upgrades_node_zoom());
+				?>
+                
+				var llHome = new GLatLng('<?=$lat?>', '<?=$lon?>');
+				map = new GMap2(document.getElementById("gmap"));
+				map.addControl(new GSmallZoomControl());
+				map.setCenter(llHome, <?=$zoom?>, G_SATELLITE_MAP);
+				map.panBy(new GSize(-282, 0));
+                
+				// Create a GIcon that will be used to mark all of the local events.
+				var Icon = new GIcon();
+				Icon.image = "<?php bloginfo('url'); ?>/wp-content/uploads/icon.png";
+				Icon.iconSize = new GSize(22, 64);
+				Icon.iconAnchor = new GPoint(11, 54);
+				Icon.infoWindowAnchor = new GPoint(11, 54);
+				
+				var home = new GMarker(llHome, { icon:Icon });
+				map.addOverlay(home);
+				
+				GEvent.addListener(home, 'click', function() { 
+					home.openInfoWindowHtml('Upcoming Event!'); 
+				});
+                
+				// Dear curious source-looker,
+				// Why doesn't it work to just pull in the feed?  It has GeoRSS info in it! :(
+				// -jeff
+				// var geoXml = new GGeoXml("http://theupgrade.net/feeds.xml");
+				// map.addOverlay(geoXml);
+                
+				<?php foreach(get_net_feed() as $fitem): if(isset($fitem['georss'])): ?>
+					var Icon<?=$fitem['nodename']?> = new GIcon();
+					Icon<?=$fitem['nodename']?>.image = "<?=$fitem['nodeurl']?>/wp-content/uploads/icon.png";
+					Icon<?=$fitem['nodename']?>.iconSize = new GSize(22, 64);
+					Icon<?=$fitem['nodename']?>.iconAnchor = new GPoint(11, 54);
+					Icon<?=$fitem['nodename']?>.infoWindowAnchor = new GPoint(11, 54);
+                    
+					<?php $parts = explode(" ", $fitem['georss']['point']); ?>
+					var ll<?=$fitem['nodename']?> = new GLatLng('<?=$parts[0]?>', '<?=$parts[1]?>');
+					var <?=$fitem['nodename']?> = new GMarker(ll<?=$fitem['nodename']?>, { icon:Icon<?=$fitem['nodename']?> });
+					map.addOverlay(<?=$fitem['nodename']?>);
+					GEvent.addListener(<?=$fitem['nodename']?>, 'click', function() { 
+						<?=$fitem['nodename']?>.openInfoWindowHtml('U! <?=$fitem['nodename']?>'); 
+					});
+					
+				<?php endif; endforeach;?>
+
+				map.setCenter(llHome, 16, G_SATELLITE_MAP);
+				map.panBy(new GSize(-282, 0));
+			} // end if(GBrowserIsCompatible())
+	    } // end load()
+	    //]]>
+		</script>
+	
+	<?php endif;  // if get_option('upgrades_use_gmap')
+}
+
+add_action ('wp_head', 'add_wphead');
+
+// Add gmap divs
+function gmap_div () {
+if(get_option('upgrades_use_gmap')): ?>
+  <body class="<?php thematic_body_class() ?> <?php lang_dir() ?>" onload="load()" onunload="GUnload()">
+  <div id="map">
+      <div id="gmap"></div>
+      <div id="stripe"></div>
+  </div>
+  <?php else: ?>
+      <body class="<?php thematic_body_class() ?> <?php lang_dir() ?>">
+      <div id="map">
+      <?php
+          $temp = $wp_query;
+          get_posts("category_name=events");
+          while (have_posts())
+          {
+              the_post();
+              $q = 'post_mime_type=image&post_parent='.$post->ID;
+              $images =& get_children($q);
+              if ( !empty($images)) {
+                  foreach( $images as $attachment_id => $attachment ) {
+                      $im = wp_get_attachment_image_src( $attachment_id, 'full' );
+                      $header_bg_image = $im[0];
+                  }
+              }
+          }
+          $wp_query = $temp;
+          ?>
+          <style>
+          #map {
+              background-image: url(<?=$header_bg_image?>);
+              background-repeat: repeat;
+          }
+          </style>
+          <div id="gmap"></div>
+          <div id="stripe"></div>
+      </div>
+	
+<?php endif;
+}
+add_action (thematic_aboveheader, gmap_div);
+
 
 
 // Determine if the language requires RTL settings (using the qTranslate plugin):
@@ -192,6 +356,9 @@ function upgrade_postfooter(){
   <?php
 }
 add_filter(thematic_postfooter, upgrade_postfooter);
+
+
+
 
 
 // Dashboard Node Settings
