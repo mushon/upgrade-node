e18693a35e12bc25c52a4b6af8e2dbd93ad06969
angelng <angel.ng.27@gmail.com>
2010-06-21 16:45:51 -0400

    Fixed searchsubmit hover state.

--- functions.php
+++ functions.php
@@ -92,142 +92,13 @@ function add_wphead() {
  
  // Echo the whole thing
  echo $content;
-
-
-// Add gmap scripts and options
-   if(get_option('upgrades_use_gmap')): ?>
-	
-	  <?php $wpgeo_options = get_option("wp_geo_options"); ?>
-	  <script src="http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=<?=$wpgeo_options['google_api_key']?>" type="text/javascript"></script>
-	    <script type="text/javascript">
-	    //<![CDATA[
-		var map;
-	    function load()
-		{
-			if (GBrowserIsCompatible())
-			{
-				<?php
-				
-				$lat = null;
-				$lon = null;
-				
-				$my_query = new WP_Query("category_name=events&showposts=1");
-				if($my_query->have_posts())
-				{
-					$my_query->the_post();
-					$lon = get_post_meta($my_query->post->ID, '_wp_geo_longitude', true);
-					$lat = get_post_meta($my_query->post->ID, '_wp_geo_latitude', true);
-				}
-				
-				if(!$lat || !$lon)
-				{
-					$lat = attribute_escape(upgrades_node_lat());
-					$lon = attribute_escape(upgrades_node_lon());
-				}
-				$zoom = attribute_escape(upgrades_node_zoom());
-				?>
-                
-				var llHome = new GLatLng('<?=$lat?>', '<?=$lon?>');
-				map = new GMap2(document.getElementById("gmap"));
-				map.addControl(new GSmallZoomControl());
-				map.setCenter(llHome, <?=$zoom?>, G_SATELLITE_MAP);
-				map.panBy(new GSize(-282, 0));
-                
-				// Create a GIcon that will be used to mark all of the local events.
-				var Icon = new GIcon();
-				Icon.image = "<?php bloginfo('url'); ?>/wp-content/uploads/icon.png";
-				Icon.iconSize = new GSize(22, 64);
-				Icon.iconAnchor = new GPoint(11, 54);
-				Icon.infoWindowAnchor = new GPoint(11, 54);
-				
-				var home = new GMarker(llHome, { icon:Icon });
-				map.addOverlay(home);
-				
-				GEvent.addListener(home, 'click', function() { 
-					home.openInfoWindowHtml('Upcoming Event!'); 
-				});
-                
-				// Dear curious source-looker,
-				// Why doesn't it work to just pull in the feed?  It has GeoRSS info in it! :(
-				// -jeff
-				// var geoXml = new GGeoXml("http://theupgrade.net/feeds.xml");
-				// map.addOverlay(geoXml);
-                
-				<?php foreach(get_net_feed() as $fitem): if(isset($fitem['georss'])): ?>
-					var Icon<?=$fitem['nodename']?> = new GIcon();
-					Icon<?=$fitem['nodename']?>.image = "<?=$fitem['nodeurl']?>/wp-content/uploads/icon.png";
-					Icon<?=$fitem['nodename']?>.iconSize = new GSize(22, 64);
-					Icon<?=$fitem['nodename']?>.iconAnchor = new GPoint(11, 54);
-					Icon<?=$fitem['nodename']?>.infoWindowAnchor = new GPoint(11, 54);
-                    
-					<?php $parts = explode(" ", $fitem['georss']['point']); ?>
-					var ll<?=$fitem['nodename']?> = new GLatLng('<?=$parts[0]?>', '<?=$parts[1]?>');
-					var <?=$fitem['nodename']?> = new GMarker(ll<?=$fitem['nodename']?>, { icon:Icon<?=$fitem['nodename']?> });
-					map.addOverlay(<?=$fitem['nodename']?>);
-					GEvent.addListener(<?=$fitem['nodename']?>, 'click', function() { 
-						<?=$fitem['nodename']?>.openInfoWindowHtml('U! <?=$fitem['nodename']?>'); 
-					});
-					
-				<?php endif; endforeach;?>
-
-				map.setCenter(llHome, 16, G_SATELLITE_MAP);
-				map.panBy(new GSize(-282, 0));
-			} // end if(GBrowserIsCompatible())
-	    } // end load()
-	    //]]>
-		</script>
-	
-	<?php endif;  // if get_option('upgrades_use_gmap')
+ 
 }
  
 add_action ('wp_head', 'add_wphead');
 
-/*
-// Add gmap divs
-function gmap_div () {
-if(get_option('upgrades_use_gmap')): ?>
-  <body class="<?php thematic_body_class() ?> <?php lang_dir() ?>" onload="load()" onunload="GUnload()">
-  <div id="map">
-      <div id="gmap"></div>
-      <div id="stripe"></div>
-  </div>
-  <?php else: ?>
-      <body class="<?php thematic_body_class() ?> <?php lang_dir() ?>">
-      <div id="map">
-      <?php
-          $temp = $wp_query;
-          get_posts("category_name=events");
-          while (have_posts())
-          {
-              the_post();
-              $q = 'post_mime_type=image&post_parent='.$post->ID;
-              $images =& get_children($q);
-              if ( !empty($images)) {
-                  foreach( $images as $attachment_id => $attachment ) {
-                      $im = wp_get_attachment_image_src( $attachment_id, 'full' );
-                      $header_bg_image = $im[0];
-                  }
-              }
-          }
-          $wp_query = $temp;
-          ?>
-          <style>
-          #map {
-              background-image: url(<?=$header_bg_image?>);
-              background-repeat: repeat;
-          }
-          </style>
-          <div id="gmap"></div>
-          <div id="stripe"></div>
-      </div>
-	
-<?php endif;
-}
-add_action (thematic_aboveheader, gmap_div);
-*/
 
-
-// Add gmap divs
+// Add gmap divs and calling the Geo Mash Up map
 function gmap_div () {
   ?><body class="<?php thematic_body_class() ?> <?php lang_dir() ?>" onload="load()" onunload="GUnload()">
   <div id="map">
@@ -246,6 +117,7 @@ function gmap_div () {
 }
 add_action (thematic_aboveheader, gmap_div);
 
+
 // Determine if the language requires RTL settings (using the qTranslate plugin):
 function lang_dir() {
 	if ( function_exists('qtrans_getLanguage')) {
@@ -258,13 +130,14 @@ function lang_dir() {
 	}
 }
 
+/*
 // Creates the links for the translations of the post. use either 'text', 'image', 'both' or 'dropdown':
 function lang_links($id) {
 	if ( function_exists('qtrans_getLanguage') ) {
 		qtrans_generateLanguageSelectCode('text', $id);
 	}
 }
-
+                                            */
 
 // Address categories by name to check if a certain category has a certain slug
 function in_category_name($name) {
@@ -275,6 +148,7 @@ function in_category_name($name) {
 	}
 	return false;
 }
+
 
 // Gets the event time (using the Event Calendar plugin):
 function ec3_schedule(){
